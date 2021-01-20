4ffb15cba6e26ba1e07d4ea6210e2f9146877251
katerina-kis <katerina-kis@yandex.ru>
2021-01-20 21:04:44 +0300

    добавление ява

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        long b = 2_000_000_000;
+        long p = 500_000_000;
+        long rest = b + p;
+                System.out.println(rest);
+    }
+}
